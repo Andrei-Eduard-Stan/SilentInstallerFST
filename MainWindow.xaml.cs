27444765828c61a0168cc9c2f772173eb782417b
Andrei-Eduard Stan <andreieduard.stan991@gmail.com>
2025-02-17 00:10:48 +0000

    Start of the UI

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,23 +1,41 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace SilentInstallerFST;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace SilentInstaller
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PreviousInstaller_Click(object sender, RoutedEventArgs e)
+        {
+            // Logic to switch to previous installer type
+            //InstallerType.Text = "Avon Data";
+        }
+
+        private void NextInstaller_Click(object sender, RoutedEventArgs e)
+        {
+            // Logic to switch to next installer type
+            //InstallerType.Text = "Head Office";
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Switch to the installation page
+            SelectionPage.Visibility = Visibility.Collapsed;
+            InstallationPage.Visibility = Visibility.Visible;
+
+            // Start installation process (you'll need to add the actual logic)
+            //StatusMessage.Text = "Installation in progress...";
+        }
+
+        private void AbortButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Logic to abort installation
+            MessageBox.Show("Installation Aborted!", "Silent Installer", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
     }
 }
