e4ce4bc39612012b87e36806481c049e337c4cde
Andrei-Eduard Stan <andreieduard.stan991@gmail.com>
2025-03-02 11:27:22 +0000

    DCU Installation + starting to add update drivers functionality

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,11 +19,11 @@ namespace SilentInstaller
         private List<InstallationStep> installationSteps;
         private int currentStepIndex = 0;
         private int currentCategoryIndex = 0;
-        private string installerPath = @"\\ph-svr-file\Software\Andrei";
+        private string installerPath = @"C:\\Users\\asta\\OneDrive - Fuller Smith and Turner\\Documents\\projects\\silentinstaller";
 
 
 
-        
+
         private void DefineInstallationSteps(Category category)
         {
             installationSteps = new List<InstallationStep>();
@@ -41,10 +41,10 @@ namespace SilentInstaller
                 installationSteps.Add(new InstallationStep("Logmein HO","msiexec", $"/i \"{installerPath}\\LMI\\LMI Head Office.msi\" /quiet DEPLOYID=01_p7xqfoq7wc6kh6vcw4d007hp3hb1mgk5bm79z INSTALLMETHOD=5 FQDNDESC=1", "data/logmein.png"));
                 installationSteps.Add(new InstallationStep("Office Suite", $"{installerPath}\\Office\\setup.exe", $"/configure \"{installerPath}\\Office\\Latest.xml\"", "data/officesetup.png" ));
                 installationSteps.Add(new InstallationStep("Mimecast","msiexec", $"/i \"{installerPath}\\Mimecast\\Mimecast.msi\" /quiet /norestart", "data/Mimecast_Logo.png"));
-                installationSteps.Add(new InstallationStep("Teams", $"{installerPath}\\Teams\\teamsbootstrapper.exe", "-p -o \"C:\\Installers\\MSTeams-x64.msix\"", "data/teams.png"));
+                installationSteps.Add(new InstallationStep("Teams", $"{installerPath}\\Teams\\teamsbootstrapper.exe", $"-p -o \"{installerPath}\\Teams\\MSTeams-x64.msix\"", "data/teams.png"));
             }
 
-            installationSteps.Add(new InstallationStep("Dell DCU", $"{installerPath}\\SupportAssist\\Dell-Command-Update-Windows-Universal-Application_9M35M_WIN_5.4.0_A00.EXE", "s", "data/dell.png"));
+            installationSteps.Add(new InstallationStep("Dell DCU", $"{installerPath}\\SupportAssist\\Dell-Command-Update-Windows-Universal-Application_9M35M_WIN_5.4.0_A00.EXE", "/s", "data/dell.png"));
 
         }
 
@@ -120,6 +120,11 @@ namespace SilentInstaller
             {
                 AppendLog($"[ERROR] Failed to restart: {ex.Message}");
             }
+        }
+
+        private void UpdateDrivers_Click(object sender,RoutedEventArgs e) {
+
+        
         }
 
         private void UpdateUI(string appName)
